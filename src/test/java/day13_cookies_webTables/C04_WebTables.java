377c22a77bade228c149b1653445c68ba3ed5e1f
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-05-24 13:15:07 +0300

    24.05.22

--- src/test/java/day13_cookies_webTables/C04_WebTables.java
+++ src/test/java/day13_cookies_webTables/C04_WebTables.java
@@ -19,9 +19,9 @@ public class C04_WebTables extends TestBase {
   7. Tablodaki sutun sayisini yazdirin
   8. Tablodaki 3.kolonu yazdirin
   9. Tabloda "First Name" i Kierra olan kisinin Salary'sini yazdirin
-10. Page sayfasinda bir method olusturun, Test sayfasindan satir ve sutun sayisini girdigimde bana datayi yazdirsin
+  10. Page sayfasinda bir method olusturun, Test sayfasindan satir ve sutun sayisini girdigimde bana datayi yazdirsin
 
-*/
+    */
    static List<WebElement> tumSatirlarWebelementList;
    static List<WebElement> basliklarWebelementiListesi;
      @Test
