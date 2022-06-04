ee7463491775814921df7952dbd11171b0af19a2
mu5tafacool <mustafakulf1@gmail.com>
2022-06-05 00:44:17 +0300

    05 06 22 00:45

--- src/test/java/day13_cookies_webTables/C01_Cookies.java
+++ src/test/java/day13_cookies_webTables/C01_Cookies.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.Cookie;
 import utilities.TestBase;
+
 import java.util.Set;
 
 public class C01_Cookies extends TestBase {
@@ -12,35 +13,35 @@ public class C01_Cookies extends TestBase {
         // 1- Amazon anasayfaya gidin
         driver.get("https://www.amazon.com");
         //2- tum cookie’leri listeleyin
-        Set<Cookie> cookiesSet= driver.manage().getCookies();
+        Set<Cookie> cookiesSet = driver.manage().getCookies();
         System.out.println(cookiesSet);
-        int sayac=1;
-        for (Cookie each:cookiesSet
+        int sayac = 1;
+        for (Cookie each : cookiesSet
         ) {
-            System.out.println(sayac+ ".ci cookie: " + each);
-            System.out.println("name :  "+each.getName());
-            System.out.println("value : "+ each.getValue());
+            System.out.println(sayac + ".ci cookie: " + each);
+            System.out.println("name :  " + each.getName());
+            System.out.println("value : " + each.getValue());
             sayac++;
         }
         //3- Sayfadaki cookies sayisinin 5’den buyuk oldugunu test edin
-        int cookieSayisi= cookiesSet.size();
-        Assert.assertTrue(cookieSayisi>5);
+        int cookieSayisi = cookiesSet.size();
+        Assert.assertTrue(cookieSayisi > 5);
         //4- ismi i18n-prefs olan cookie degerinin USD oldugunu test edin
-        for (Cookie each: cookiesSet
+        for (Cookie each : cookiesSet
         ) {
-            if (each.getName().equals("i18n-prefs")){
+            if (each.getName().equals("i18n-prefs")) {
                 Assert.assertEquals("USD", each.getValue());
             }
         }
         //5- ismi “en sevdigim cookie” ve degeri “cikolatali” olan
         // bir cookie olusturun ve sayfaya ekleyin
-        Cookie cookie= new Cookie("en sevdigim cookie","cikolatali");
+        Cookie cookie = new Cookie("en sevdigim cookie", "cikolatali");
         driver.manage().addCookie(cookie);
-        sayac=1;
-        cookiesSet= driver.manage().getCookies();
-        for (Cookie each:cookiesSet
+        sayac = 1;
+        cookiesSet = driver.manage().getCookies();
+        for (Cookie each : cookiesSet
         ) {
-            System.out.println(sayac+ ".ci cookie: " + each);
+            System.out.println(sayac + ".ci cookie: " + each);
             sayac++;
         }
         //6- eklediginiz cookie’nin sayfaya eklendigini test edin
@@ -50,7 +51,7 @@ public class C01_Cookies extends TestBase {
         Assert.assertFalse(cookiesSet.contains("skin"));
         //8- tum cookie’leri silin ve silindigini test edin
         driver.manage().deleteAllCookies();
-        cookiesSet= driver.manage().getCookies();
+        cookiesSet = driver.manage().getCookies();
         Assert.assertTrue(cookiesSet.isEmpty());
     }
 }
