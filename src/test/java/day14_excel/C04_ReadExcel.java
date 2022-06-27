832212ad6da92320c3105e3ee7de6cccc9d3d20d
mu5tafacool <mustafakulf1@gmail.com>
2022-06-28 00:11:08 +0300

    28 06 22 00:15

--- src/test/java/day14_excel/C04_ReadExcel.java
+++ src/test/java/day14_excel/C04_ReadExcel.java
@@ -3,6 +3,7 @@ package day14_excel;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.Test;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,18 +12,18 @@ public class C04_ReadExcel {
     @Test
     public void readExcelTest() throws IOException {
         //7. Dosya yolunu bir String degiskene atayalim
-        String dosyaYolu="src/resources/ulkeler.xlsx";
+        String dosyaYolu = "src/resources/ulkeler.xlsx";
         //8. FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
-        FileInputStream fis=new FileInputStream(dosyaYolu);
+        FileInputStream fis = new FileInputStream(dosyaYolu);
         //9. Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
         //10. WorkbookFactory.create(fileInputStream)
-        Workbook workbook= WorkbookFactory.create(fis);
+        Workbook workbook = WorkbookFactory.create(fis);
         // ingilizce baskentler sutununu yazdiralim
-        int sonSatir= workbook.getSheet("Sayfa1").getLastRowNum();
+        int sonSatir = workbook.getSheet("Sayfa1").getLastRowNum();
         System.out.println(sonSatir);
-        String satirdakiData="";
-        for (int i = 0; i <=sonSatir; i++) {
-            satirdakiData= C03_ReadExcel.banaDataGetir(i,1);
+        String satirdakiData = "";
+        for (int i = 0; i <= sonSatir; i++) {
+            satirdakiData = C03_ReadExcel.banaDataGetir(i, 1);
             System.out.println(satirdakiData);
         }
     }
